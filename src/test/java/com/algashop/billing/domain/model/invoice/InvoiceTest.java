a5e95f30e023cad99df111054b59b9f9402b5e2d
Amauri Castro <amauri.p.castro@gmail.com>
2026-01-13 08:34:17 -0300

    correcao: adicionado arquivos alterados para o commit anterior

--- src/test/java/com/algashop/billing/domain/model/invoice/InvoiceTest.java
+++ src/test/java/com/algashop/billing/domain/model/invoice/InvoiceTest.java
@@ -1,4 +1,147 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.algashop.billing.domain.model.invoice;
+
+import com.algashop.billing.domain.model.DomainException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
 class InvoiceTest {
-  
+
+    @Test
+    public void shouldIssueInvoice() {
+        Invoice invoice = Invoice.issue(
+                "01226N0693HDA",
+                UUID.randomUUID(),
+                InvoiceTestDataBuilder.aPayer(),
+                Set.of(InvoiceTestDataBuilder.aLineItem())
+        );
+
+        Assertions.assertThat(invoice.getId()).isNotNull();
+        Assertions.assertThat(invoice.getTotalAmount()).isNotNull();
+        Assertions.assertThat(invoice.getIssuedAt()).isNotNull();
+        Assertions.assertThat(InvoiceStatus.UNPAID).isEqualTo(invoice.getStatus());
+
+    }
+
+    @Test
+    public void shouldMarkAsPaid() {
+        Invoice invoice = InvoiceTestDataBuilder.anInvoice().build();
+
+        invoice.markAsPaid();
+
+        Assertions.assertThat(invoice.getStatus()).isEqualTo(InvoiceStatus.PAID);
+        Assertions.assertThat(invoice.getIssuedAt()).isNotNull();
+    }
+
+    @Test
+    public void shouldCancelWithReason() {
+        Invoice invoice = InvoiceTestDataBuilder.anInvoice().build();
+
+        String cancelReason = "The customer regretted the purchase.";
+        invoice.cancel(cancelReason);
+
+        Assertions.assertThat(invoice.getStatus()).isEqualTo(InvoiceStatus.CANCELED);
+        Assertions.assertThat(invoice.getCanceledAt()).isNotNull();
+        Assertions.assertThat(invoice.getCancelReason()).isEqualTo(cancelReason);
+    }
+
+    @Test
+    public void shouldIssueInvoiceCorrectly() {
+        String orderId = "123";
+        UUID customerId = UUID.randomUUID();
+        Payer payer = InvoiceTestDataBuilder.aPayer();
+        Set<LineItem> items = new HashSet<>();
+        items.add(InvoiceTestDataBuilder.aLineItem());
+        items.add(InvoiceTestDataBuilder.aLineItemAlt());
+
+        Invoice invoice = Invoice.issue(orderId, customerId, payer, items);
+
+        BigDecimal expectedTotalAmount = invoice.getItems()
+                .stream()
+                .map(LineItem::getAmount)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        Assertions.assertWith(invoice,
+                i -> Assertions.assertThat(i.getId()).isNotNull(),
+                i -> Assertions.assertThat(i.getTotalAmount()).isEqualTo(expectedTotalAmount),
+                i -> Assertions.assertThat(i.getStatus()).isEqualTo(InvoiceStatus.UNPAID)
+        );
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenIssuingInvoiceWithEmptyItems() {
+        Set<LineItem> emptyItems = new HashSet<>();
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> Invoice.issue("01226N0693HDA23",
+                        UUID.randomUUID(),
+                        InvoiceTestDataBuilder.aPayer(),
+                        emptyItems));
+    }
+
+    @Test
+    public void shouldChangePaymentSettingsWhenUnpaid() {
+        Invoice invoice = InvoiceTestDataBuilder.anInvoice().build();
+        UUID creditCardId = UUID.randomUUID();
+        invoice.changePaymentSettings(PaymentMethod.CREDIT_CARD, creditCardId);
+        Assertions.assertWith(invoice,
+                i -> Assertions.assertThat(i.getPaymentSettings()).isNotNull(),
+                i -> Assertions.assertThat(i.getPaymentSettings().getMethod()).isEqualTo(PaymentMethod.CREDIT_CARD),
+                i -> Assertions.assertThat(i.getPaymentSettings().getCreditCardId()).isEqualTo(creditCardId));
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenChangingPaymentSettingsToPaidInvoice() {
+        Invoice invoice = InvoiceTestDataBuilder.anInvoice().status(InvoiceStatus.PAID).build();
+        Assertions.assertThatExceptionOfType(DomainException.class)
+                .isThrownBy(() -> invoice.changePaymentSettings(PaymentMethod.CREDIT_CARD, UUID.randomUUID()));
+    }
+
+
+
+    @Test
+    public void shouldThrowExceptionWhenMarkingCanceledInvoiceAsPaid() {
+        Invoice invoice = InvoiceTestDataBuilder.anInvoice().status(InvoiceStatus.CANCELED).build();
+
+        Assertions.assertThatExceptionOfType(DomainException.class)
+                .isThrownBy(invoice::markAsPaid);
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenCancelingAlreadyCanceledInvoice() {
+        Invoice invoice = InvoiceTestDataBuilder.anInvoice().status(InvoiceStatus.CANCELED).build();
+
+        Assertions.assertThatExceptionOfType(DomainException.class)
+                .isThrownBy(() -> invoice.cancel("Another reason"));
+    }
+
+    @Test
+    public void shouldAssignPaymentGatewayCodeWhenUnpaid() {
+        Invoice invoice = InvoiceTestDataBuilder.anInvoice().paymentSettings(PaymentMethod.CREDIT_CARD, UUID.randomUUID()).build();
+        String gatewayCode = "code-from-gateway";
+
+        invoice.assignPaymentGatewayCode(gatewayCode);
+
+        Assertions.assertThat(invoice.getPaymentSettings().getGatewayCode()).isEqualTo(gatewayCode);
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenAssigningGatewayCodeToPaidInvoice() {
+        Invoice invoice = InvoiceTestDataBuilder.anInvoice().status(InvoiceStatus.PAID).build();
+        Assertions.assertThatExceptionOfType(DomainException.class)
+                .isThrownBy(() -> invoice.assignPaymentGatewayCode("some-code"));
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenTryingToModifyItemsSet() {
+        Invoice invoice = InvoiceTestDataBuilder.anInvoice().build();
+        Set<LineItem> items = invoice.getItems();
+
+        Assertions.assertThatExceptionOfType(UnsupportedOperationException.class)
+                .isThrownBy(items::clear);
+    }
+
 }
