33b9fea6d4ccb54eb5b159d8aa514b4764c16633
Amauri Castro <amauri.p.castro@gmail.com>
2026-07-18 12:05:45 -0300

    aprimorado auditoria com SecurityCheckApplicationService

--- src/main/java/com/algashop/billing/infrastructure/persistence/SpringDataAuditConfig.java
+++ src/main/java/com/algashop/billing/infrastructure/persistence/SpringDataAuditConfig.java
@@ -1,5 +1,6 @@
 package com.algashop.billing.infrastructure.persistence;
 
+import com.algashop.billing.application.security.SecurityCheckApplicationService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.auditing.DateTimeProvider;
@@ -24,7 +25,12 @@ public class SpringDataAuditConfig {
     }
 
     @Bean
-    public AuditorAware<UUID> auditorProvider() {
-        return () -> Optional.of(UUID.randomUUID());
+    public AuditorAware<UUID> auditorProvider(SecurityCheckApplicationService securityCheck) {
+        return () -> {
+            if (!securityCheck.isAuthenticated() || securityCheck.isMachineAuthenticated()) {
+                return Optional.empty();
+            }
+            return Optional.of(securityCheck.getAuthenticatedUserId());
+        };
     }
 }
